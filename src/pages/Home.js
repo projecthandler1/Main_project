4fbbe503163b1f71dbab9882bcf88899b54a0e6d
projecthandler1 <120641821+projecthandler1@users.noreply.github.com>
2022-12-26 22:40:17 +0530

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Faqs from '../components/Home/Faqs'
 import GlassMorphism from '../components/Home/GlassMorphism'
+import FirstIm from '../components/Home/FirstIm'
+import Sender from '../components/Home/Sender'
+import Info from '../components/Home/Info'
 
 function Home() {
   return (
     <div>
+    <FirstIm/>
+    <Sender/>
+    <Info/>
     <GlassMorphism/>
     <Faqs/>
     </div>
